0d8a059c666d35575118f0a283c5de61054e600b
Aarsh <aarsh@optimustech.io>
2020-09-19 23:08:35 +0530

    Index for hari mostly.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 const product = require('./schema/Product');
 
-console.log(product);
+// ====================================================================
+// Do not turn this, unless it is the first time you're running it
+// ====================================================================
+// require('./uploadData');
+
+// ====================================================================
+// Entered below are 2 equivalent queries for elasticsearch and mongodb
+// . The elastic search does not really work here, but try it on dev
+// tool. To make it work here, I was thinking we could directly use the
+// elasticsearch API. Much more efficient.
+
+// I think elasticsearch also has a built-in caching mechanism. Because
+// The first result does take time to generate but eventually it reduces
+// by one exponent. That could also be kibana, but not sure.
+// ====================================================================
+
+const searchString = 'dacosamite';
+
+// Elastic results
+product.search(
+  {
+    query_string: {
+      query: {
+        bool: {
+          should: [
+            {
+              fuzzy: {
+                brand: {
+                  value: searchString,
+                  fuzziness: 'AUTO',
+                  max_expansions: 20,
+                },
+              },
+            },
+            {
+              fuzzy: {
+                name: {
+                  value: searchString,
+                  fuzziness: 'AUTO',
+                  max_expansions: 20,
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
+  (err, res) => {
+    if (err) console.log(err);
+    else {
+      const results = res.hits.hits;
+      console.log(results);
+    }
+  },
+);
+
+// ====================================================================
+// I did prepare one on my compass but, it did't have fuzzzy search so,
+// no effing point doing it.
+// ====================================================================
+product.aggregate([]);
